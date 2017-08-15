7a5e11feea57d5bfa07b0a47ca410f99eb4248ca
robinsonkwame <kporterrobinson@gmail.com>
2017-08-14 20:21:09 -0400

    WIP: adds yes to installation,untested

--- install/setup.sh
+++ install/setup.sh
@@ -3,16 +3,16 @@
 # Assumes AWS, firewall on, otherwise this machine may expose unncessary ports
 # to the intertubes...
 sudo apt update
-sudo apt-get install build-essential python-dev git
+sudo -y apt-get install build-essential python-dev git
 
 # Python
-sudo apt-git install python-pip python-virtualenv python-numpy python-matplotlib
+sudo -y apt-git install python-pip python-virtualenv python-numpy python-matplotlib
 
 # Redis
-sudo apt-get install redis-server
+sudo -y apt-get install redis-server
 
 # Install mongodb
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
+sudo -y apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo -y apt-get update
+sudo -y apt-get install -y mongodb-org
