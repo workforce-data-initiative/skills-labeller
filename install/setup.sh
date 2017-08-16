cb9bb74a39665ddc2263e4e3bba6e092153d708f
robinsonkwame <kporterrobinson@gmail.com>
2017-08-16 16:07:51 -0400

    updates setup to reflect use of mlabs instead of local mongo

--- install/setup.sh
+++ install/setup.sh
@@ -11,8 +11,9 @@ sudo -y apt-git install python-pip python-virtualenv python-numpy python-matplot
 # Redis
 sudo -y apt-get install redis-server
 
-# Install mongodb
-sudo -y apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-sudo echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo -y apt-get update
-sudo -y apt-get install -y mongodb-org
+# Use AWS MLabs mongodb service, 500 mb free, offloads Mongodb to a service
+## Install mongodb
+#sudo -y apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+#sudo echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+#sudo -y apt-get update
+#sudo -y apt-get install -y mongodb-org
