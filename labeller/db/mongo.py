3a4ac47de5d9d002190c59757a81deff280dbf72
Gregory Mundy <gregmundy@gmail.com>
2017-08-21 01:50:18 -0400

    fixed mongodb object to load data from a remote server

--- labeller/db/mongo.py
+++ labeller/db/mongo.py
@@ -15,13 +15,22 @@ from bson.objectid import ObjectId
 class MongoDatabase(object):
     """ MongoDB ORM. """
 
-    __slots__ = ['MONGO_HOST', 'MONGO_PORT', 'MONGO_DATABASE', 'client', 'db']
+    # __slots__ = ['MONGO_HOST', 'MONGO_PORT', 'MONGO_DATABASE',
+    #              'MONGO_USERNAME', 'MONGO_PASSWORD' 'client', 'db']
 
     def __init__(self):
         self.MONGO_HOST = os.getenv('MONGO_HOST', 'localhost')
         self.MONGO_PORT = os.getenv('MONGO_PORT', 27017)
         self.MONGO_DATABASE = os.getenv('MONGO_DATABASE', 'skills-labeller')
-        self.client = pymongo.MongoClient(self.MONGO_HOST, self.MONGO_PORT)
+        self.MONGO_USERNAME = os.getenv('MONGO_USERNAME', None)
+        self.MONGO_PASSWORD = os.getenv('MONGO_PASSWORD', None)
+
+        if not self.MONGO_USERNAME or not self.MONGO_PASSWORD:
+            self.client = pymongo.MongoClient(
+                self.MONGO_HOST, int(self.MONGO_PORT))
+        else:
+            connection = "mongodb://{0}:{1}@{2}:{3}/{4}".format(self.MONGO_USERNAME, self.MONGO_PASSWORD, self.MONGO_HOST, self.MONGO_PORT, self.MONGO_DATABASE)
+            self.client = pymongo.MongoClient(connection)
         self.db = self.client[self.MONGO_DATABASE]
 
     def create_job_posting(self, posting):
@@ -56,7 +65,7 @@ class MongoDatabase(object):
         """
 
         count = self.db.job_postings.count()
-        index = random.randrange(1, count-1)
+        index = random.randrange(1, count - 1)
         sample = self.db.job_postings.find().limit(-1).skip(index).next()
         sample['_id'] = str(sample['_id'])
         return sample
