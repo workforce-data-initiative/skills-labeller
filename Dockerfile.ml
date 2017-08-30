a3fce96a072739e301397fa0f7a630170640f89d
robinsonkwame <kporterrobinson@gmail.com>
2017-08-30 19:26:57 -0400

    WIP: Dockerfile for ML, need to figure out why openjdk not available

--- Dockerfile.ml
+++ Dockerfile.ml
@@ -1,7 +1,35 @@
-FROM ubuntu:14.04
-FROM openjdk:7-jdk
+FROM python:3.6.2-slim
 MAINTAINER "Kwame Porter Robinson" kporterrobinson@gmail.com
 
+# Install JDK
+# Credit: picoded/ubuntu-openjdk-8-jdk
+
+# Add some core repos
+RUN apt-get update && \
+    apt-get install -y sudo curl zip openssl build-essential python-software-properties && \
+    apt-get clean;
+
+# This is in accordance to : https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-get-on-ubuntu-16-04
+RUN apt-get update && \
+    apt-get install -y openjdk-8-jdk && \
+    apt-get install -y ant && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* && \
+    rm -rf /var/cache/oracle-jdk8-installer;
+
+# Fix certificate issues, found as of # https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/983302
+RUN apt-get update && \
+    apt-get install -y ca-certificates-java && \
+    apt-get clean && \
+    update-ca-certificates -f && \
+    rm -rf /var/lib/apt/lists/* && \
+    rm -rf /var/cache/oracle-jdk8-installer;
+
+# Setup JAVA_HOME, this is useful for docker commandline
+ENV JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64/
+RUN export JAVA_HOME
+
+# Install Vowpal Wabbit
 RUN apt-get update &&\
     apt-get -y install git build-essential libboost-program-options-dev libboost-python-dev zlib1g-dev  &&\
     git clone git://github.com/JohnLangford/vowpal_wabbit.git /vowpal_wabbit &&\
@@ -11,14 +39,6 @@ RUN apt-get update &&\
 
 RUN apt-get clean -y && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-# Install Python packages (really only need pymongo though)
-RUN apt-get -y install python3-pip
-ADD requirements requirements
-ADD requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt --no-cache-dir
-RUN apt-get update && apt-get -y autoremove
-
-# Copy over Oracle script
 COPY ./labeller/skill-oracle/run.sh /vowpal_wabbit
 RUN chmod a+x run.sh
 ENTRYPOINT ["/vowpal_wabbit/run.sh"]
