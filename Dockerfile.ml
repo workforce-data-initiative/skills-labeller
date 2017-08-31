7eeab64556aa45fa57a44a6e39e2d9ee5779e7cc
robinsonkwame <kporterrobinson@gmail.com>
2017-08-31 13:02:43 -0400

    WIP: adds java includes file to Vowpal Wabbit build (I really shouldn't have to much with compile options...)

--- Dockerfile.ml
+++ Dockerfile.ml
@@ -6,39 +6,26 @@ MAINTAINER "Kwame Porter Robinson" kporterrobinson@gmail.com
 
 # Add some core repos
 RUN apt-get update && \
-    apt-get install -y sudo curl zip openssl build-essential python-software-properties && \
+    apt-get install -y sudo curl zip openssl build-essential python-software-properties software-properties-common && \
     apt-get clean;
 
-# This is in accordance to : https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-get-on-ubuntu-16-04
-RUN apt-get update && \
-    apt-get install -y openjdk-8-jdk && \
-    apt-get install -y ant && \
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/* && \
-    rm -rf /var/cache/oracle-jdk8-installer;
-
-# Fix certificate issues, found as of # https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/983302
-RUN apt-get update && \
-    apt-get install -y ca-certificates-java && \
-    apt-get clean && \
-    update-ca-certificates -f && \
-    rm -rf /var/lib/apt/lists/* && \
-    rm -rf /var/cache/oracle-jdk8-installer;
-
-# Setup JAVA_HOME, this is useful for docker commandline
-ENV JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64/
-RUN export JAVA_HOME
+RUN echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+RUN echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+RUN apt-get update
+RUN apt-get install -y openjdk-7-jdk
+RUN JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
 
 # Install Vowpal Wabbit
 RUN apt-get update &&\
     apt-get -y install git build-essential libboost-program-options-dev libboost-python-dev zlib1g-dev  &&\
     git clone git://github.com/JohnLangford/vowpal_wabbit.git /vowpal_wabbit &&\
-    cd /vowpal_wabbit && make && make install &&\
+    cd /vowpal_wabbit && make -I$(JAVA_HOME)/include -I$(JAVA_HOME)/include/linux && make install &&\
     rm -Rf /vowpal_wabbit/* &&\
     apt-get -y autoremove
 
 RUN apt-get clean -y && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-COPY ./labeller/skill-oracle/run.sh /vowpal_wabbit
-RUN chmod a+x run.sh
-ENTRYPOINT ["/vowpal_wabbit/run.sh"]
+#COPY ./labeller/skill-oracle/run.sh /vowpal_wabbit
+#RUN chmod a+x run.sh
+#ENTRYPOINT ["/vowpal_wabbit/run.sh"]
