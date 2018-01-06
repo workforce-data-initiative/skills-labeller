17c5e609e42a59af3a7e221c0d466d14eca14bde
robinsonkwame <kporterrobinson@gmail.com>
2018-01-06 11:28:04 -0500

    WIP: I think I have the docker-compose up line working here. Note that docker ps shows nothing when the test harness finishes, but putting a break point in the unit test shows, with docker ps, that it's all stood up as expected, so I guess pytest shuts it down on finishing; it's possible the spawned process is still a child process even though I thought I detached it.

--- test/test_skilloracle_api.py
+++ test/test_skilloracle_api.py
@@ -3,18 +3,66 @@ import time
 import unittest
 import requests
 import json
+import os
+import subprocess
+
+class DockerCompose(object):
+    """
+    Helper class for interacting with DockerCompose via subprocess
+    """
+    def __init__(self):
+        self.program = 'docker-compose'
+        self.version_string = '--version'
+        self.up = 'up'
+
+    def get_version(self):
+        try:
+            ret = subprocess.check_output([self.program, self.version_string],
+                                          stderr=subprocess.STDOUT)
+        except OSError:
+            raise OSError("docker-compose must be installed!") from OSError
+
+        # get the version number, assuming the following format ... ... version, ...
+        ret = ret.decode("utf").split()[2][:-1]
+
+        return ret
+
+    def run(self, path="../", yml="docker-compose.yml", service="skilloracle", cmd=None):
+        if yml:
+            yml_path = os.path.join(os.path.dirname(__file__),
+                                    path,
+                                    yml)
+            yml_path = "-f " + yml_path
+        # see: https://stackoverflow.com/a/34459371/3662899,
+        # want to deattach from the Python process to reflect how a standalone API
+        # would really behave
+
+        shell_command = " ".join([self.program, yml_path, cmd, service])
+        ret = subprocess.Popen(shell_command,
+                               shell=True,
+                               close_fds=True)
+
+        return ret._child_created
 
 class TestSkillOracleAPI(unittest.TestCase):
     """ Unit test for Skill Oracle API """
 
-    def setUp(self, port=7000, host='127.0.0.1'):
-        self.port = port
-        self.host = host
-        self.redis = "redis"
+    def setUp(self):
+        self.dockercompose = DockerCompose()
 
-        # todo:
-        # check that docker-compose exists, is of right version
+    def test_check_version(self):
+        """
+        This is a very basic version check and isn't robust against
+        letters and other ways of indicating versions. There are python
+        libraries offering better version testing to consider using.
+        """
+        ret = self.dockercompose.get_version()
+        split_version = ret.split('.')
+        assert int(split_version[0]) >= 1,\
+            "Major Docker-compose version is too low ({})".format(split_version[0])
 
-    def test_mytest(self):
+        assert int(split_version[1]) >= 10,\
+            "Minor Docker-compose version is too low ({})".format(split_version[1])
 
-        return True
+    def test_up(self):
+        assert self.dockercompose.run(cmd='up'), "Was not able to run docker-compose up"
