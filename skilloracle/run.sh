3cc3fa0eded72f76946b12ca8ab824ff5420ea48
robinsonkwame <kporterrobinson@gmail.com>
2018-02-20 16:52:11 -0500

    WIP: adds __pycache__ to .dockerignore, adds .dockerignore (never really needed because unit tested against container output

--- skilloracle/run.sh
+++ skilloracle/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [ "$RUN_UNITTESTS_ONLY" = true ];
+#then bash
 then pytest --full-trace test/test_skilloracle.py
 else python3 webserver.py # todo: update with nameko run service or supervisord
 fi
