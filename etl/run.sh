6861210cecb183f0885db40ed4fb036af0e26605
robinsonkwame <kporterrobinson@gmail.com>
2018-03-08 17:51:21 -0500

    WIP: curl does not seem easily installable in the container (getting apt-get 404 messages), todo: figure out another way to test if ampq is back up w/o using curl or get curl installed

--- etl/run.sh
+++ etl/run.sh
@@ -1,10 +1,25 @@
+#!/bin/bash
+
+# adopted from nameko-example
+
+is_ready() {
+    eval "curl -I http://${RABBIT_USER}:${RABBIT_PASSWORD}@${RABBIT_HOST}:${RABBIT_MANAGEMENT_PORT}/api/vhosts"
+}
+
 if [ "$RUN_UNITTESTS_ONLY" = true ];
 then pytest --full-trace test/test_preprocessor.py test/test_candidate_skills.py test/test_etl.py
-#else python vt.py # should provide access to endpoint or resources on which to stand up 1 or more endpoints
-#else nameko run vt
-# hold open so i can test this code, using nameko
-else while :; do echo 'Hit Ctrl+C'; sleep 25; done
+else
+	i=0
+	while ! is_ready; do
+		i=`expr $i + 1`
+		if [ $i -ge 10 ]; then
+			echo "$(date) - rabbit still not ready, giving up"
+			exit 1
+		fi
+		echo "$(date) - waiting for rabbit to be ready"
+		sleep 3
+	done
+	nameko run --config etl/config.yml etl.vt
 fi
-
 # trick for keeping container open, for debug
-# while :; do echo 'Hit Ctrl+C'; sleep 25; done
+#else while :; do echo 'Hit Ctrl+C'; sleep 25; done
